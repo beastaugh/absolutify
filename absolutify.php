00bfbf1070bc91471fb087340268254c239c63e5
Benedict Eastaugh <benedict@eastaugh.net>
2008-10-28 13:30:09 +0000

    Absolutify comment content URLs as well as post and page ones

--- absolutify.php
+++ absolutify.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Absolutify
 Plugin URI: http://extralogical.net/
-Description: Turn relative URIs in your posts and pages into absolute ones, fixing the issues you'll otherwise have when your content is loaded in a context outside your site.
+Description: Turn relative URIs in posts, pages and comments into absolute ones, fixing the issues you'll otherwise have when your content is loaded in a context outside your site.
 Author: Benedict Eastaugh
 Version: 1.0
 Author URI: http://extralogical.net/
@@ -17,5 +17,6 @@ function absolutify_content_urls($content) {
 }
 
 add_filter('the_content', 'absolutify_content_urls');
+add_filter('comment_text', 'absolutify_content_urls');
 
 ?>
