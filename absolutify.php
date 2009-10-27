e3a0e6e3e283c96d76bc722caa09c459c49e0279
Benedict Eastaugh <benedict@eastaugh.net>
2009-10-27 13:43:19 +0000

    Spaces, not tabs; improve the documentation a little.

--- absolutify.php
+++ absolutify.php
@@ -2,18 +2,28 @@
 /*
 Plugin Name: Absolutify
 Plugin URI: http://extralogical.net/
-Description: Turn relative URIs in posts, pages and comments into absolute ones, fixing the issues you'll otherwise have when your content is loaded in a context outside your site.
+Description: Turn relative URIs in posts, pages and comments into absolute ones.
 Author: Benedict Eastaugh
-Version: 1.0
+Version: 1.1
 Author URI: http://extralogical.net/
-.
-Works for links using both single and double quotes. Also handy if you ever move your domain.
-.
 */
 
+/**
+ * Turns relative URIs into absolute ones.
+ *
+ * This plugin fixes the issues you'll otherwise have when using relative URIs
+ * if your content is loaded in a context outside your site. Works for links
+ * using both single and double quotes. It can also be handy if you ever move
+ * your domain.
+ *
+ * @uses get_bloginfo
+ *
+ * @param string $content
+ * @return string
+ */
 function absolutify_content_urls($content) {
-	$url = get_bloginfo('url');
-	return preg_replace("/href=(\"|')\//", "href=\${1}$url/", $content);
+    $url = get_bloginfo('url');
+    return preg_replace("/href=(\"|')\//", "href=\${1}$url/", $content);
 }
 
 add_filter('the_content', 'absolutify_content_urls');
